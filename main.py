19519c6253f41b6286e03fd6047cb79fd57fcefc
ompatel11 <ompatel1105@outlook.com>
2020-04-21 02:19:28 +0530

    Created MDCard grid.

--- main.py
+++ main.py
@@ -8,25 +8,16 @@ from kivymd.theming import ThemableBehavior
 from kivymd.uix.card import MDCard
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.label import MDLabel
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList
 
-class Cards(MDCard):
-    
-    def create_cards(self):
-        pass
+class Cards():
+    pass
 class calling_functions(MDScreen):
     def __init__(self, *args, **kwargs):
         super(calling_functions, self).__init__(**kwargs)
         self.no_files = 5
-        self.lists_fl = []
-        for i in range(self.no_files):
-            k = Cards(id=f"{i}")
-            self.lists_fl.append(k)
-        self.lists_fl[0].pos_hint = {'bottom':1}
-        for j in range(5):
-            
-            self.lists_fl[j].add_widget(MDLabel(text="Hi World"))
-            self.ids.md_list.add_widget(self.lists_fl[j])
-             
+    
     def change_screen(self):
         if self.ids.screen_manager.current == "homescreen":
             self.ids.screen_manager.current = "new_task"  
